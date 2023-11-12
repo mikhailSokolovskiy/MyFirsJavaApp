d189c7c10a31ec04e539cc917fd8076c8b465b72
Mikhail <mikhail.sokolovskiy.2002@mail.ru>
2023-11-12 17:50:45 +0500

    drawing 90 triangle

--- src/lr4/Example2.java
+++ src/lr4/Example2.java
@@ -1,4 +1,21 @@
 package lr4;
 
 public class Example2 {
+    public static void main(String[] args) {
+        int rows = 10;
+        int cols = 10;
+
+        int i = 0;
+        while (i < rows) {
+            int j = 0;
+            while (j < cols) {
+                if (j <= i) {
+                    System.out.print("*");
+                }
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
 }
